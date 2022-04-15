a57181cae01e1658da5b591f007b699721936507
pregan23 <pregan2bing@gmail.com>
2022-04-15 16:19:39 -0400

    get streets comments

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
 
 Router.post('/:id/:streetid/new', controller.CreateComment)
+Router.get('/:streetid', controller.GetStreetsComments)
 
 module.exports = Router
