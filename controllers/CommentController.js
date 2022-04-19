ea05a30e7b28497a20a15a405f1e5441744049d1
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-04-19 09:31:51 -0400

    Removed Console Logs

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,53 +1,53 @@
 const { Comment, CommentLike, User } = require('../models')
 
 const CreateComment = async (req, res) => {
-    try {
-        let authorId = parseInt(req.params.id)
-        let streetId = parseInt(req.params.streetid)
-        let commentBody = {
-            authorId,
-            ...req.body,
-            streetId
-        }
-        let newComment = await Comment.create(commentBody)
-        res.send(newComment)
-    } catch (error) {
-        throw error
+  try {
+    let authorId = parseInt(req.params.id)
+    let streetId = parseInt(req.params.streetid)
+    let commentBody = {
+      authorId,
+      ...req.body,
+      streetId
     }
+    let newComment = await Comment.create(commentBody)
+    res.send(newComment)
+  } catch (error) {
+    throw error
+  }
 }
 
 const GetStreetsComments = async (req, res) => {
-    try {
-        let streetId = req.params.streetid
-        let streetsComments = await Comment.findAll({
-            where:{
-                streetId
-            },
-            include: User
-        })
-        res.send(streetsComments)
-    } catch (error) {
-        throw error
-    }
+  try {
+    let streetId = req.params.streetid
+    let streetsComments = await Comment.findAll({
+      where: {
+        streetId
+      },
+      include: User
+    })
+    res.send(streetsComments)
+  } catch (error) {
+    throw error
+  }
 }
 
 const LikeComment = async (req, res) => {
-    try {
-        let userId = req.params.id
-        let commentId = req.params.commentid
-        let likeBody = {
-            userId,
-            commentId
-        }
-        let newCommentLike = await CommentLike.create(likeBody)
-        res.send(newCommentLike)
-    }   catch (error) {
-        throw error
+  try {
+    let userId = req.params.id
+    let commentId = req.params.commentid
+    let likeBody = {
+      userId,
+      commentId
     }
+    let newCommentLike = await CommentLike.create(likeBody)
+    res.send(newCommentLike)
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
-    CreateComment,
-    GetStreetsComments,
-    LikeComment
-} 
+  CreateComment,
+  GetStreetsComments,
+  LikeComment
+}
--- controllers/StreetController.js
+++ controllers/StreetController.js
@@ -2,28 +2,28 @@ const { user } = require('pg/lib/defaults')
 const { Street, StreetLike, User } = require('../models')
 
 const CreateStreet = async (req, res) => {
-    try {
-        let authorId = parseInt(req.params.id)
-        let streetBody = {
-            authorId,
-            ...req.body
-        }
-        let newStreet = await Street.create(streetBody)
-        res.send(newStreet)
-    } catch (error) {
-        throw error
+  try {
+    let authorId = parseInt(req.params.id)
+    let streetBody = {
+      authorId,
+      ...req.body
     }
+    let newStreet = await Street.create(streetBody)
+    res.send(newStreet)
+  } catch (error) {
+    throw error
+  }
 }
 
 const GetAllStreets = async (req, res) => {
-    try{
-        const streets = await Street.findAll({
-            include: User
-        })
-        res.send(streets)
-    }catch (error) {
-        throw error
-    }
+  try {
+    const streets = await Street.findAll({
+      include: User
+    })
+    res.send(streets)
+  } catch (error) {
+    throw error
+  }
 }
 
 // const GetLikedStreets = async (req, res) => {
@@ -35,22 +35,22 @@ const GetAllStreets = async (req, res) => {
 // }
 
 const LikeStreet = async (req, res) => {
-    try {
-        let userId = req.params.id
-        let streetId = req.params.streetid
-        let likeBody = {
-            userId,
-            streetId
-        }
-        let newStreetLike = await StreetLike.create(likeBody)
-        res.send(newStreetLike)
-    }   catch (error) {
-        throw error
+  try {
+    let userId = req.params.id
+    let streetId = req.params.streetid
+    let likeBody = {
+      userId,
+      streetId
     }
+    let newStreetLike = await StreetLike.create(likeBody)
+    res.send(newStreetLike)
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
-    CreateStreet,
-    GetAllStreets,
-    LikeStreet
+  CreateStreet,
+  GetAllStreets,
+  LikeStreet
 }
