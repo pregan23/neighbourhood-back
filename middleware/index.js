c08178e960345d2eda8c998d2be07400c30f8811
Dan Ventura <danice.ventura@gmail.com>
2022-04-19 10:11:20 -0600

    basic delete added to comments

--- middleware/index.js
+++ middleware/index.js
@@ -6,9 +6,9 @@ const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const APP_SECRET = process.env.APP_SECRET
 
 const hashPassword = async (password) => {
-    let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
-    return hashedPassword
-  }
+  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+  return hashedPassword
+}
 
 const comparePassword = async (storedPassword, password) => {
   let passwordMatch = await bcrypt.compare(password, storedPassword)
@@ -16,44 +16,45 @@ const comparePassword = async (storedPassword, password) => {
 }
 
 const createToken = (payload) => {
-    let token = jwt.sign(payload, APP_SECRET)
-    return token
-  }
+  let token = jwt.sign(payload, APP_SECRET)
+  return token
+}
 
-  const verifyToken = (req, res, next) => {
-    const { token } = res.locals
-    
-    try {
-        console.log(`=====================================================================================,${APP_SECRET}`)
-      let payload = jwt.verify(token, APP_SECRET)
-      
-      if (payload) {
-        res.locals.payload=payload
-        return next()
+const verifyToken = (req, res, next) => {
+  const { token } = res.locals
 
-      }
-      res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
-    } catch (error) {
-      res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  try {
+    console.log(
+      `=====================================================================================,${APP_SECRET}`
+    )
+    let payload = jwt.verify(token, APP_SECRET)
+
+    if (payload) {
+      res.locals.payload = payload
+      return next()
     }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
   }
-  const stripToken = (req, res, next) => {
-    try {
-      const token = req.headers['authorization'].split(' ')[1]
-      
-      if (token) {
-        res.locals.token = token
-        return next()
-      }
-    } catch (error) {
-      res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
-    }
-  }
+}
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1]
 
-  module.exports = {
-    stripToken,
-    verifyToken,
-    createToken,
-    comparePassword,
-    hashPassword
+    if (token) {
+      res.locals.token = token
+      return next()
+    }
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
   }
+}
+
+module.exports = {
+  stripToken,
+  verifyToken,
+  createToken,
+  comparePassword,
+  hashPassword
+}
